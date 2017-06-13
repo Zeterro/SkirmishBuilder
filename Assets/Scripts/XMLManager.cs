0d4d6170bb9cdad6228a58ad0ecaeb657bc04ade
Samuel Duval <samuel.k.duval@gmail.com>
2017-06-13 15:45:15 +0200

    Added bare bones for XML serialization

---
+++ Assets/Scripts/XMLManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class XMLManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+}
